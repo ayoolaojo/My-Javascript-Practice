e3af3bf22c7f1c4d8f8382cd1202dbe96beb31d4
ayoolaojo <johnsonayoolaojo@gmail.com>
2024-12-13 17:18:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("demo").innerHTML =  "My First Javascript code";
+
+
+document.getElementById('demo').style.color = 'blue';
+
+
+function myMul(x,y,z) {
+  return(x*y*z);
+}
+
+let a = myMul(2,5,4);
+console.log(a)
